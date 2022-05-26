5f63d70af0319238241d1ce955a73085f657ba4f
Théophile Wallez <theophile@wallez.net>
2022-05-26 21:08:28 +0200

    Use everest-nix

--- flake.nix
+++ flake.nix
@@ -5,14 +5,19 @@
       url = "github:FStarLang/FStar";
       flake = false;
     };
+
+    everest = {
+      url = github:project-everest/everest-nix?dir=projects;
+      inputs.fstar-src.follows    = "fstar-src";
+    };
   };
 
-  outputs = {self, nixpkgs, fstar-src}:
+  outputs = {self, nixpkgs, fstar-src, everest}:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-    z3 = pkgs.callPackage ./z3.nix {};
-    fstar = (pkgs.callPackage ./fstar.nix {inherit z3;}).binary-of-fstar { src = fstar-src; name = "fstar"; };
+    z3 = everest.packages.${system}.z3;
+    fstar = everest.packages.${system}.fstar;
     comparse = pkgs.callPackage ./default.nix {inherit fstar z3;};
   in {
     packages.${system} = {
